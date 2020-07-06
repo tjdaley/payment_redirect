86bbc251c04f79380d42c626316536d306777a25
Tom Daley <tjd@powerdaley.com>
2020-07-06 06:00:03 -0500

    provide consistent list of authorization names

---
+++ app/util/authorizations.py
@@ -0,0 +1,14 @@
+"""
+authorizatins.py - Provide authorizations information.
+
+Coyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
+"""
+AUTH_DOWNLOAD_CLIENTS = 'DOWNLOAD_CLIENTS'
+AUTH_SEND_EVERGREEN = 'SEND_EVERGREEN'
+AUTH_TEMPLATE_ADMIN = 'TEMPLATE_ADMIN'
+
+AUTHORIZATIONS = [
+    {'key': AUTH_DOWNLOAD_CLIENTS, 'description': "Download client lists"},
+    {'key': AUTH_SEND_EVERGREEN, 'description': "Send evergreen letters"},
+    {'key': AUTH_TEMPLATE_ADMIN, 'description': "Administer email templates"}
+]
