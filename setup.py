cb9328b75d7a9f4ca7525acb3aafd0beefc29b54
Tom Daley <tjd@powerdaley.com>
2020-07-26 01:22:40 -0500

    add new file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="attorneycrm-pkg-tjdaley",
+    version="0.0.1",
+    author="Thomas J. Daley, J.D.",
+    author_email="tom@powerdaley.com",
+    description="Attorney Client Relationship Management App",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tjdaley/payment_redirect",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD-3-Clause",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
