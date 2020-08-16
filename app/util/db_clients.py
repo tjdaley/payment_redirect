a81382fb49df48638f7f561416a0b608039482bb
Tom Daley <tjd@powerdaley.com>
2020-08-16 01:05:41 -0500

    add billing_id to transform

--- app/util/db_clients.py
+++ app/util/db_clients.py
@@ -369,6 +369,7 @@ def intake_to_client(intake: dict) -> dict:
         client_doc['active_flag'] = 'Y'
         client_doc['admin_users'] = admin.get('AssignedTo', os.environ.get('ADMIN_USERS'))
         client_doc['crm_state'] = admin.get('CRMState', '040"consult_scheduled')
+        client_doc['billing_id'] = admin.get('BillingID', '9999')
 
         # About the Cliente
         about_you = intake.get('AboutYou', {})
