a553dba62e94eeca205d8810c68dfe09a5e8a12e
Tom Daley <tjd@powerdaley.com>
2020-08-16 00:43:29 -0500

    add default admin users from config

--- app/views/crm/crm_routes.py
+++ app/views/crm/crm_routes.py
@@ -404,7 +404,7 @@ def update_intake():
         client_doc['_id'] = '0'
         client_doc['crm_state'] = '040:consult_scheduled'
         client_doc['active_flag'] = 'Y'
-        client_doc['admin_users'] = ['tdaley@koonsfuller.com']
+        client_doc['admin_users'] = os.environ.get('ADMIN_USERS', '')
         logger.debug("About to save")
         result = DBCLIENTS.save(client_doc, 'tdaley@koonsfuller.com')
         logger.debug("Save done . . .")
