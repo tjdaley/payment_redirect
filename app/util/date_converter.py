a675b1b14c5b1a505205a47742edcab4bd739412
Tom Daley <tjd@powerdaley.com>
2020-09-19 22:33:03 -0500

    add new validator

---
+++ app/util/date_converter.py
@@ -0,0 +1,37 @@
+"""
+date_converter.py - Custom Validator for Dates
+
+Copyright (c) 2020 by Thomas J. Daley, J.D. All Rights Reserved.
+"""
+from wtforms import ValidationError
+from datetime import date
+
+
+class DateConverter(object):
+    """
+    This class is a custom validator for WTForms. Use it to convert datetime.date
+    data to string before saving to Mongo DB.
+    """
+    def __call__(self, form, field):
+        """
+        Args:
+            form (Form): Referencee to form that is hosting the field
+            field (DateField): Reference to field that is being fixed
+        Returns:
+            None
+
+        Returns:
+            ValidationError if the input value doesn't clean up properly.
+
+        Side-Effects:
+            datetime.date is converted to YYYY-MM-DD string.
+        """
+        if not isinstance(field.data, (date)):
+            print("Skipping conversion for", type(field.data))
+            return
+
+        try:
+            field.data = field.data.strftime('%Y-%m-%d')
+            print("Converted date to", field.data)
+        except ValueError as e:
+            raise ValidationError(str(e))
