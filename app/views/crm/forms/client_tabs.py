f188364ecf9d28bdb5d7ef58c4e11a997b2b1716
Tom Daley <tjd@powerdaley.com>
2021-01-10 09:47:27 -0600

    add new plan view

--- app/views/crm/forms/client_tabs.py
+++ app/views/crm/forms/client_tabs.py
@@ -15,89 +15,82 @@ default_li_class = 'nav-item'
 default_a_class = 'nav-link'
 
 # Tabs will appear in the order below.
-client_tabs = [
-    {
-        'li_class': default_li_class,
-        'a_class': 'nav-link active',
-        'name': 'name',
-        'label': "Name",
-        'selected': 'true'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'address',
-        'label': "Address",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'contact',
-        'label': "Contact",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'case',
-        'label': "Case",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'contacts',
-        'label': "Players",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'children',
-        'label': "Children",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'billing',
-        'label': "Billing",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'id',
-        'label': "ID",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'dates',
-        'label': "Dates",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'insurance',
-        'label': "Insurance",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'marketing',
-        'label': "Marketing",
-        'selected': 'false'
-    },
-    {
-        'li_class': default_li_class,
-        'a_class': default_a_class,
-        'name': 'access',
-        'label': "Access",
-        'selected': 'false'
-    }
-]
+client_tabs = [{
+    'li_class': default_li_class,
+    'a_class': 'nav-link active',
+    'name': 'name',
+    'label': "Name",
+    'selected': 'true'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'address',
+    'label': "Address",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'contact',
+    'label': "Contact",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'case',
+    'label': "Case",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'contacts',
+    'label': "Players",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'children',
+    'label': "Children",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'billing',
+    'label': "Billing",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'id',
+    'label': "ID",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'dates',
+    'label': "Dates",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'plan',
+    'label': "Plan",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'insurance',
+    'label': "Insurance",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'marketing',
+    'label': "Marketing",
+    'selected': 'false'
+}, {
+    'li_class': default_li_class,
+    'a_class': default_a_class,
+    'name': 'access',
+    'label': "Access",
+    'selected': 'false'
+}]
